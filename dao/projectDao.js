9a65c1a67f2725051ead3af2450c61b95fa55d0b
raxkumar-wedaa <rakesh.c@wedaa.tech>
2023-09-15 21:37:54 +0530

    added project APIs

---
+++ dao/projectDao.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+const projectSchema = require('../model/project');
+
+projectSchema.statics = {
+
+    create: function (data) {
+        var project = new this(data);
+        return project.save();
+    },
+
+    get: function (query) {
+        return this.find(query)
+            .lean()
+            .then((results) => {
+                if (results && results.length > 0) {
+                    return results.map((result) => ({
+                        id: result._id,
+                        name: result.name
+                    })) || [];
+                }
+            });
+    },
+
+    getByProjectId: function (query) {
+        return this.find(query)
+            .lean()
+            .then((results) => {
+                if (results && results.length > 0) {
+                    return results.map((result) => ({
+                        id: result._id,
+                        name: result.name
+                    })) || [];
+                }
+            });
+    },
+
+    update: function (query, updateData) {
+        return this.findOneAndUpdate(query, { $set: updateData }, { new: true });
+    },
+
+    delete: function (query) {
+        return this.deleteOne(query);
+    },
+
+}
+
+var projectModel = mongoose.model('project', projectSchema);
+module.exports = projectModel;
