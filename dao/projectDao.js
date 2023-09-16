7b246970b45731eb97a292f024163266a6915637
Dheeraj-S001 <saidheeraj.gantala@gmail.com>
2023-09-16 12:17:04 +0530

    Added blueprints coount to get call

--- dao/projectDao.js
+++ dao/projectDao.js
@@ -2,38 +2,49 @@ var mongoose = require('mongoose');
 const projectSchema = require('../model/project');
 
 projectSchema.statics = {
-
     create: function (data) {
         var project = new this(data);
         return project.save();
     },
 
-    get: function (query) {
-        return this.find(query)
-            .lean()
-            .then((results) => {
-                if (results && results.length > 0) {
-                    return results.map((result) => ({
-                        id: result._id,
-                        name: result.name,
-                        imageUrl: result.imageUrl,
-                        description: result.description
-                    })) || [];
-                }
-            });
+    get: async function () {
+        try {
+            const projects = await this.find().lean();
+
+            if (projects && projects.length > 0) {
+                const projectPromises = projects.map(async project => {
+                    const blueprintCount = await blueprintModel.countDocuments({
+                        parentId: project._id,
+                    });
+                    return {
+                        id: project._id,
+                        name: project.name,
+                        blueprintCount: blueprintCount,
+                    };
+                });
+
+                return Promise.all(projectPromises);
+            } else {
+                return [];
+            }
+        } catch (error) {
+            throw error;
+        }
     },
 
     getByProjectId: function (query) {
         return this.find(query)
             .lean()
-            .then((results) => {
+            .then(results => {
                 if (results && results.length > 0) {
-                    return results.map((result) => ({
-                        id: result._id,
-                        name: result.name,
-                        imageUrl: result.imageUrl,
-                        description: result.description
-                    })) || [];
+                    return (
+                        results.map(result => ({
+                            id: result._id,
+                            name: result.name,
+                            imageUrl: result.imageUrl,
+                            description: result.description,
+                        })) || []
+                    );
                 }
             });
     },
@@ -45,8 +56,7 @@ projectSchema.statics = {
     delete: function (query) {
         return this.deleteOne(query);
     },
-
-}
+};
 
 var projectModel = mongoose.model('project', projectSchema);
 module.exports = projectModel;
