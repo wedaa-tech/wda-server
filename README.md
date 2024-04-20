7d3e0c1143f4341d1ee862192df14e1f70150971
raxkumar-wedaa <rakesh.c@wedaa.tech>
2024-04-20 18:35:23 +0530

    update the readme

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This backend service needs below components to function properly:
 
 -   MongoDB
 -   Keycloak
+-   RabbitMQ
 
 Run Keycloak as docker instance:
 
@@ -21,14 +22,27 @@ Run MongoDB as docker instance:
 npm run docker:db:up
 ```
 
+Run RabbitMQ as docker instance:
+
+```
+npm run docker:rabbitmq:up
+```
+
 ## How to run in development ?
 
-```
-node server.js
-```
+1. Create a copy of the .env.example file with the name .env [First Time Only]
 
-or
+2. Add the API Keys for any 3rd party libraries, for example:
+    - FLAGSMITH_API_KEY
 
-```
-npm start
-```
+3. Start the server
+
+    ```
+    node server.js
+    ```
+
+    or
+
+    ```
+    npm start
+    ```
--- src/configs/feature-flag.js
+++ src/configs/feature-flag.js
@@ -4,6 +4,13 @@ const flagsmith = new Flagsmith({
     environmentKey: process.env.FLAGSMITH_API_KEY,
 });
 
+/**
+ * Checks if one or multiple flags are enabled.
+ * @param {string|string[]} flags - A single flag or an array of flags to check.
+ * @returns {Object|boolean} - If a single flag is passed, returns a boolean indicating if the flag is enabled.
+ *                             If an array of flags is passed, returns an object with each flag mapped
+ *                             to its corresponding boolean value indicating if it is enabled.
+ */
 async function checkFlagsEnabled(flags) {
     const allFlags = await flagsmith.getEnvironmentFlags();
     const flagsResult = {};
